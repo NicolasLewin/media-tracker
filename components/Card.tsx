7c87c048e1524658a8a98e346113ad3734556189
NicolasLewin <nicolas.lewin@outlook.com>
2024-08-09 19:00:20 +0200

    added images for cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,7 +12,7 @@ export const Card = (props: CardProps) => {
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img className="rounded-t-lg" src={props.image} alt="" />
+                <img className="rounded-t-lg" src={props.image} alt="sa" />
             </a>
             <div className="p-5">
                 <a href="#">
--- components/Main.tsx
+++ components/Main.tsx
@@ -7,8 +7,8 @@ export const Main = () => {
         <div>
             <h1 className="text-2xl">Welcome to media tracker !</h1>
             <div className="flex gap-4 justify-center">
-                <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={""} path={""} />
-                <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={""} path={""} />
+                <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={"/images/gaming1.jpg"} path={""} />
+                <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={""} />
                 <Card title={"Compare with your friends!"} description={"Compare and see what your friends have seen and/or played!"} image={""} path={""} />
             </div>
         </div>
