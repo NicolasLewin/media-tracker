6f325512b9522986e3b9559199ebbfe58eb73c43
NicolasLewin <nicolas.lewin@outlook.com>
2024-08-12 23:49:40 +0200

    removed howlongtobeat dependency

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,10 +1,8 @@
 import { Header } from "@/components/Header";
-import { Main } from "@/components/Main";
 import { Spacing } from "@/components/Spacing";
-import Image from "next/image";
 
 export default function Games() {
-  return (
+    return (
     <main>
         <Header />
         <Spacing height={100} />
