2454cb5fe4ff3db0e9f098af9ba29aa4fc7fbf78
NicolasLewin <nicolas.lewin@outlook.com>
2024-08-14 21:15:16 +0200

    removed unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 import { Spacing } from "@/components/Spacing";
-import Image from "next/image";
 
 export default function Home() {
   return (
