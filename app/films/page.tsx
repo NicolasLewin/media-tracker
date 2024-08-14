ca9e33ed52db64057e63c1330aea311a0233e710
NicolasLewin <nicolas.lewin@outlook.com>
2024-08-14 21:15:49 +0200

    removed unused imports

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,7 +1,5 @@
 import { Header } from "@/components/Header";
-import { Main } from "@/components/Main";
 import { Spacing } from "@/components/Spacing";
-import Image from "next/image";
 
 export default function Films() {
   return (
