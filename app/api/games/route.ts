53f410889246f8f7abf4bc7ecf7baa351d39eb00
NicolasLewin <nicolas.lewin@outlook.com>
2024-09-12 23:25:21 +0200

    handle fetching error and use of Game type

--- app/api/games/route.ts
+++ app/api/games/route.ts
@@ -1,18 +1,43 @@
-import { NextRequest, NextResponse } from "next/server";
+import { Game } from '@/types';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: any) {
-
-    const url = "https://api.igdb.com/v4/games"
-
-    const res = await fetch(url, { method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Client-ID': '',
-          'Authorization': 'Bearer ',
-        },
-    body: "fields name; where id = 1942;"
+export async function GET(request: Request) {
+  const url = "https://api.igdb.com/v4/games";
+  
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Client-ID': '',
+        'Authorization': 'Bearer ',
+      },
+      body: 'fields name,summary,cover.*; where id = 1942; limit 1;'
     });
-    const data = await res.json();
-    return NextResponse.json({ data });
 
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    const game = data[0];
+
+    const coverUrl = (() => {
+      if (game.cover?.image_id)
+        return `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`
+      return null
+    })()
+
+    const gameData: Game = {
+      id: game.id,
+      name: game.name,
+      summary: game.summary,
+      coverUrl: coverUrl
+    };
+
+    return NextResponse.json(gameData);
+  } catch (error) {
+    console.error('Error fetching game data:', error);
+    return NextResponse.json({ error: 'Error fetching game data' }, { status: 500 });
+  }
 }
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -2,16 +2,18 @@
 
 import { Header } from "@/components/Header";
 import { Spacing } from "@/components/Spacing";
-import { useEffect, useState } from "react";
+import { Game } from "@/types";
+import { useState } from "react";
 
 export default function Games() {
 
-    const [game, setGame] = useState([])
+    const [game, setGame] = useState<Game | null>(null)
 
     async function fetchGames() {
       try {
         const response = await fetch("http://localhost:3000/api/games");
-        const jsonData = (await response.json()).data;
+        const jsonData = (await response.json()) as Game;
+        console.log(jsonData);
         setGame(jsonData);
       } catch (e) {
         console.error("Error while fetching:", e);
@@ -24,11 +26,8 @@ export default function Games() {
         <Spacing height={100} />
         <div>
           <button onClick={fetchGames}>Search</button>
-          {game.map(game =>
-             <div className="flex">
-                {game.name}
-              </div>
-          )}
+          { game && game.coverUrl && <img src={game.coverUrl} alt="alt"/> }
+          <img src={game?.coverUrl ?? ''} />
         </div>
     </main>
   );
