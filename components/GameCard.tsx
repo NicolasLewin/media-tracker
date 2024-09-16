b2325f3cf56b2990da2c309ece0a66b595fa3a56
NicolasLewin <nicolas.lewin@outlook.com>
2024-09-16 23:41:41 +0200

    fixed GameCard to check if game has cover

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -1,27 +1,28 @@
 "use client"
 
-import Image from "next/image";
+import { Game } from "@/types";
 
 export type GameCardProps = {
-    id: string;
-    title: string;
-    cover: string;
+    game: Game;
 }
-
 
 export const GameCard = (props: GameCardProps) => {
     return (
-        <div key={props.id} className="bg-white flex flex-col h-full rounded-lg shadow-lg hover:bg-slate-100 hover:cursor-pointer">
-            
+        <div key={props.game.id} className="bg-white flex flex-col h-full rounded-lg shadow-lg hover:bg-slate-100 hover:cursor-pointer">
             <div className="flex-grow flex flex-col items-center justify-center p-2">
-            
+            {props.game.cover && props.game.cover.url ? (
                 <img
-                    src={props.cover.replace('t_thumb', 't_cover_big')}
-                    alt={`${props.title} cover`}
+                    src={props.game.cover.url.replace('t_thumb', 't_cover_big')}
+                    alt={`${props.game.name} cover`}
                     className="rounded-lg shadow-lg max-w-full h-auto"
                     style={{ maxHeight: '300px' }}
                 />
-                <p className="text-lg text-center">{props.title}</p>
+            ) : (
+                <div className="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center">
+                  <span className="text-gray-400">No image available</span>
+                </div>
+              )}
+                <p className="text-lg text-center">{props.game.name}</p>
             </div>
         </div>
     );
