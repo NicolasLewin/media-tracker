d6b4e8007c544a0a34572738a0357016e06ba3c6
NicolasLewin <nicolas.lewin@outlook.com>
2024-10-02 23:53:26 +0200

    updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,6 +27,8 @@ export const Header = () => {
           const data = await response.json();
           if (data.isLoggedIn) {
             setUser(data.user);
+          } else {
+            setUser(null);
           }
         } catch (error) {
           console.error('Error checking login status:', error);
@@ -58,11 +60,10 @@ export const Header = () => {
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Media Tracker</span>
             </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                {!user &&
-                    <button type="button" onClick={toggleLoginRegisterModal} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-                }
-                {user &&
+                {user ?
                     <button type="button" onClick={handleLogout} className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Logout</button>
+                :
+                    <button type="button" onClick={toggleLoginRegisterModal} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
                 }
                 {isLoginRegisterModalOpen && <LoginRegisterModal onClose={toggleLoginRegisterModal} />}
                 <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
