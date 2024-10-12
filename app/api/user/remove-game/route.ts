a157477c56831f26ab9c3237debc3a330cae59a8
NicolasLewin <nicolas.lewin@outlook.com>
2024-10-12 12:26:59 +0200

    added remove game route

---
+++ app/api/user/remove-game/route.ts
@@ -0,0 +1,43 @@
+import prisma from '@/prisma'
+import { NextResponse } from 'next/server'
+import jwt from 'jsonwebtoken'
+
+export async function POST(request: Request) {
+  try {
+    const token = request.cookies.get('token')?.value
+
+    if (!token) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+    }
+
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(token, process.env.JWT_SECRET) as { userId: number }
+    } catch (error) {
+      return NextResponse.json({ message: 'Invalid token' }, { status: 401 })
+    }
+
+    const { gameId } = await request.json()
+    
+    const game_api_id = String(gameId)
+
+    const deletedGame = await prisma.userGame.deleteMany({
+      where: {
+        user_id: decodedToken.userId,
+        game_api_id: game_api_id,
+      },
+    })
+
+    if (deletedGame.count === 0) {
+      return NextResponse.json({ message: 'Game not found in user profile' }, { status: 404 })
+    }
+
+    return NextResponse.json({ message: 'Game removed from profile' }, { status: 200 })
+
+  } catch (error) {
+    console.error('Error removing game from profile:', error)
+    return NextResponse.json({ message: 'Error removing game from profile' }, { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
