9c99379d019d5678a3f7ef244829a38d498e2f7c
NicolasLewin <nicolas.lewin@outlook.com>
2024-10-13 17:17:18 +0200

    added movie type

--- types.ts
+++ types.ts
@@ -1,7 +1,14 @@
 export type Game = {
-    id: string,
-    name: string,
-    cover?: {
-      url: string
-    };
+  id: string,
+  name: string,
+  cover?: {
+    url: string
+  };
+}
+
+export type Movie = {
+  imdbID: string,
+  Title: string,
+  Year: string,
+  Poster: string
 }
