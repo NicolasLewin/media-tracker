0f94e036e7f9fc607bbe6b1883a15b41eb2d0ca7
NicolasLewin <nicolas.lewin@outlook.com>
2024-11-13 23:56:37 +0100

    changed path in main

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,7 +7,7 @@ export const Main = () => {
         <div>
             <div className="flex gap-4 justify-center">
                 <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={"/images/gaming1.jpg"} path={"games"} />
-                <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"films"} />
+                <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"movies"} />
             </div>
         </div>
 
