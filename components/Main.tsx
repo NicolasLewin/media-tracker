a45ebe52d909ae30851ba0f8a07aa48f9d130333
NicolasLewin <nicolas.lewin@outlook.com>
2024-11-13 23:47:02 +0100

    removed card

--- components/Main.tsx
+++ components/Main.tsx
@@ -5,14 +5,9 @@ import { Card } from "@/components/Card";
 export const Main = () => {
     return (
         <div>
-            <div className="flex justify-center">
-                <h1 className="text-2xl">Welcome to media tracker !</h1>
-            </div>
-
             <div className="flex gap-4 justify-center">
                 <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={"/images/gaming1.jpg"} path={"games"} />
                 <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"films"} />
-                <Card title={"Compare with your friends!"} description={"Compare and see what your friends have seen and/or played!"} image={""} path={""} />
             </div>
         </div>
 
