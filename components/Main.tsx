ec4b74d65ea44605a03e34027877161b5acb0ed8
NicolasLewin <nicolas.lewin@outlook.com>
2025-01-05 23:54:40 +0100

    fixed responsive Cards

--- components/Main.tsx
+++ components/Main.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/Card";
 export const Main = () => {
     return (
         <div>
-            <div className="flex gap-4 justify-center">
+            <div className="flex flex-col md:flex-row gap-4 justify-center items-center md:items-stretch">
                 <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={"/images/gaming1.jpg"} path={"games"} />
                 <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"movies"} />
             </div>
