6c532eefa66836d9718aecf81e2ace929399c76d
NicolasLewin <nicolas.lewin@outlook.com>
2025-01-10 16:40:10 +0100

    added null checking

--- app/movies/[movieId]/page.tsx
+++ app/movies/[movieId]/page.tsx
@@ -101,39 +101,47 @@ export default function MovieDetailsPage({ params }: { params: { movieId: string
               <div className="flex flex-col md:flex-row md:space-x-8">
                 <div className="md:w-1/3 mb-6 md:mb-0">
                   <img 
-                    src={movie.Poster !== "N/A" ? movie.Poster : "/placeholder.png"} 
-                    alt={movie.Title} 
+                    src={movie?.Poster !== "N/A" ? movie?.Poster : "/placeholder.png"} 
+                    alt={movie?.Title} 
                     className="w-full h-auto object-cover rounded-lg shadow-md"
                   />
                 </div>
                 <div className="md:w-2/3">
-                  <h1 className="text-3xl font-extrabold text-gray-900 mb-4">{movie.Title}</h1>
+                  <h1 className="text-3xl font-extrabold text-gray-900 mb-4">{movie?.Title}</h1>
                   <div className="space-y-6">
                     <div>
-                      <p className="text-xl font-semibold">Rating: <span className="font-normal">{movie.imdbRating} / 10</span></p>
+                      <p className="text-xl font-semibold">Rating: <span className="font-normal">{movie?.imdbRating} / 10</span></p>
                     </div>
-                    <div>
-                      <h2 className="text-xl font-semibold mb-2">Plot</h2>
-                      <p className="text-gray-700">{movie.Plot}</p>
-                    </div>
-                    <div>
-                      <h2 className="text-xl font-semibold mb-2">Genre</h2>
-                      <div className="flex flex-wrap gap-2">
-                        {movie.Genre.split(', ').map(genre => (
-                          <span key={genre} className="px-2 py-1 bg-blue-100 text-blue-800 rounded-full text-sm">
-                            {genre}
-                          </span>
-                        ))}
+                    {movie?.Plot && (
+                      <div>
+                        <h2 className="text-xl font-semibold mb-2">Plot</h2>
+                        <p className="text-gray-700">{movie.Plot}</p>
                       </div>
-                    </div>
+                    )}
+                    {movie?.Genre && (
+                      <div>
+                        <h2 className="text-xl font-semibold mb-2">Genre</h2>
+                        <div className="flex flex-wrap gap-2">
+                          {movie.Genre.split(', ').map(genre => (
+                            <span key={genre} className="px-2 py-1 bg-blue-100 text-blue-800 rounded-full text-sm">
+                              {genre}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                    {movie?.Director && (
                     <div>
                       <h2 className="text-xl font-semibold mb-2">Director</h2>
                       <p className="text-gray-700">{movie.Director}</p>
                     </div>
+                    )}
+                    {movie?.Actors && (
                     <div>
                       <h2 className="text-xl font-semibold mb-2">Actors</h2>
                       <p className="text-gray-700">{movie.Actors}</p>
                     </div>
+                    )}
                     <div className="flex">
                       <div>
                         <button 
