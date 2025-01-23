3983819587ac0972d555e7bc1555697190f00f59
NicolasLewin <nicolas.lewin@outlook.com>
2025-01-23 22:18:54 +0100

    updated texts

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,7 +7,7 @@ export const Main = () => {
         <div>
             <div className="flex flex-col md:flex-row gap-4 justify-center items-center md:items-stretch">
                 <Card title={"Find your games!"} description={"Countless games are present, keep track of your completion!"} image={"/images/gaming1.jpg"} path={"games"} />
-                <Card title={"What about films and series?"} description={"How many films/series did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"movies"} />
+                <Card title={"What about movies?"} description={"How many movies did you see? Find them and add them to your profile!"} image={"/images/movie-theater.jpg"} path={"movies"} />
             </div>
         </div>
 
