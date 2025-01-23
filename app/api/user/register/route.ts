a154c7479302e048e88041a273220e4c94a2f592
NicolasLewin <nicolas.lewin@outlook.com>
2025-01-23 22:11:52 +0100

    added password verifications

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -17,9 +17,14 @@ export async function POST(request: NextRequest) {
       }
     })
 
-    if (existingUser) {
+    if (existingUser)
       return NextResponse.json({ message: 'User already exists' }, { status: 400 })
-    }
+
+    if (/[@#$%^&*(),.":{}|<>]/.test(password))
+      return NextResponse.json({ message: 'Password has an unsupported character' }, { status: 400 })
+
+    if(password.length < 8)
+      return NextResponse.json({ message: 'Password must be at least 8 characters' }, { status: 400 })
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
